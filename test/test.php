3121c4d885b30465c03061576121e79e37e33d14
nicework <wenqinchao@gmail.com>
2024-03-06 00:01:13 +0800

    a

--- test/test.php
+++ test/test.php
@@ -4,7 +4,7 @@ use Qin\Web3Php\account\Account;
 
 
 
-$account = new Account("80f5785770e616fcd186ebf0613cdd461d238952c0f57d85cec31e5e42700f11");
+$account = new Account();
 var_dump($account->privateKey->hex());
 var_dump($account->privateKey->publicKey->hex());
 var_dump($account->address);
