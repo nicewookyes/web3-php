29b66b80919aa65e27c077bfff203dceaf696c03
nicework <aaaaa@gmail.com>
2024-03-05 14:37:00 +0800

    a

--- src/account/Account.php
+++ src/account/Account.php
@@ -3,4 +3,8 @@ namespace Qin\Web3Php\account;
 class Account{
     public $privateKey;
     public $publicKey;
+
+    public function create($extra=''){
+
+    }
 }
--- src/test.php
+++ src/test.php
@@ -2,9 +2,11 @@
 
 
 
-require_once('eth_utils/Conversions.php');
-use Qin\Web3Php\eth_utils\Conversions;
+//require_once('eth_utils/Conversions.php');
+//use Qin\Web3Php\eth_utils\Conversions;
+//
+//var_dump(Conversions::to_bytes("你好啊"));
+//echo "\n";
+//var_dump(Conversions::to_bytes("Hello"));
 
-var_dump(Conversions::to_bytes("你好啊"));
-echo "\n";
-var_dump(Conversions::to_bytes("Hello"));
+var_dump(random_bytes(32));
